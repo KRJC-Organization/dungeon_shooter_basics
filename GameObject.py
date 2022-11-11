5fcdcb1511d42e96d81e04b0ddab44f0314d1ab3
jgdml <jgdml2801@gmail.com>
2022-11-11 10:43:55 -0300

    basics

---
+++ GameObject.py
@@ -0,0 +1,22 @@
+from pygame import Rect
+from Axis import Axis
+
+
+class GameObject:
+    def __init__(self, x, y, speed, sprite):
+        self.x = x
+        self.y = y
+        self.speed = speed
+        if sprite is not None:
+            self.sprite = sprite.convert_alpha()
+
+    def get_rect(self):
+        size = self.sprite.get_size()
+        return Rect(self.x, self.y, size[0], size[1])
+
+    def render(self, screen):
+        screen.blit(self.sprite, [self.x, self.y])
+
+    def get_sprite_size(self):
+        size = self.sprite.get_size()
+        return Axis(size[0], size[1])
